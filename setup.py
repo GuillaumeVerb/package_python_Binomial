51aa35e1e956aa84f33c3853c08d09bb141c8ee8
Guillaume Verbiguie <Guillaume.Verbiguie@ipsos.com>
2021-02-18 21:45:52 +0100

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_gv_01',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_gv_01'],
+      zip_safe=False)
